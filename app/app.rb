8787efbf7688c340c2cb4178431fb64691eee725
MoeSadoon <msadoon590@gmail.com>
2015-09-24 11:43:20 +0100

    welcome message

--- app/app.rb
+++ app/app.rb
@@ -43,7 +43,7 @@ class Controller < Sinatra::Base
   end
 
   post '/users' do
-    User.create(email: params[:email],
+    user = User.create(email: params[:email],
                 password: params[:password])
     session[:user_id] = user.id
     redirect to('/links')
